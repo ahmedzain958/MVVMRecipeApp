e6a365556f2572f87ceac2ee58bcd29339b3cfab
Zain <ahmed.zaineldin@vodafone.com>
2023-10-18 21:19:48 +0300

    fix hilt issues

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/components/RecipeCard.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/components/RecipeCard.kt
@@ -1,30 +1,79 @@
 package com.codingwithmitch.mvvmrecipeapp.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import com.codingwithmitch.mvvmrecipeapp.R
 import com.codingwithmitch.mvvmrecipeapp.domain.model.Recipe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 
+@ExperimentalCoroutinesApi
 @Composable
 fun RecipeCard(
     recipe: Recipe,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     Card(
         shape = MaterialTheme.shapes.small,
         modifier = Modifier
             .padding(
                 bottom = 6.dp,
-                top = 6.dp
+                top = 6.dp,
             )
             .fillMaxWidth()
-            .clickable(onClick = onClick)
+            .clickable(onClick = onClick),
+        elevation = 8.dp,
     ) {
 
+        Column() {
+            recipe.featuredImage?.let { url ->
+                Image(
+                    painter = painterResource(id = R.drawable.empty_plate),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(225.dp) ,
+                    contentScale = ContentScale.Crop,
+                )
+            }
+            recipe.title?.let { title ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp)
+                ) {
+                    Text(
+                        text = title,
+                        modifier = Modifier
+                            .fillMaxWidth(0.85f)
+                            .wrapContentWidth(Alignment.Start),
+                        style = MaterialTheme.typography.h5
+                    )
+                    val rank = recipe.rating.toString()
+                    Text(
+                        text = rank,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.End)
+                            .align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+            }
+        }
     }
 }
